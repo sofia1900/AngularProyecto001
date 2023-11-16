ef6fc422c4e05146dc97508ca23ebb10ec69ff26
sofia1900 <sofia.jimlap@educa.jcyl.es>
2023-11-16 22:44:07 +0100

    Ejercicio varios componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {LibrosComponent} from "./componentes/libros/libros.component";
+import {SociosComponent} from "./componentes/socios/socios.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LibrosComponent, SociosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'proyecto001';
 }
